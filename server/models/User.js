fe98c3b16991fc75b01a1fa8c9bc1d3424edd203
matheo-gif <56673761+matheo-gif@users.noreply.github.com>
2023-08-03 11:54:34 +0300

    dashboard commit

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 100,
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 5,
+
+    },
+    city: {
+        type: String,
+        min: 2,
+
+
+    },
+    county: {
+        type: String,
+        min: 2,
+    },
+    occupation: String,
+    phoneNumber: String,
+    transactions: Array,
+    role: {
+        type: String,
+        enum: ["user", "admin", "superadmin"],
+        default: "admin"
+    },
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
